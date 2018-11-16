0861c88e5c86e8a55ceae2252f11b7dd040e12d1
Miro Sheytanov <mirosheytanov@gmail.com>
2018-11-16 09:56:27 +0200

    update

--- js/pong.js
+++ js/pong.js
@@ -1,17 +1,6 @@
 const canvas = document.getElementById("pong");
 const ctx = canvas.getContext('2d');
 
-// Load sounds
-const hit = new Audio();
-const wall = new Audio();
-const userScore = new Audio();
-const comScore = new Audio();
-
-hit.src = "assets/sounds/pong/hit.mp3";
-wall.src = "assets/sounds/pong/wall.mp3";
-comScore.src = "assets/sounds/pong/comScore.mp3";
-userScore.src = "assets/sounds/pong/userScore.mp3";
-
 // Ball object
 const ball = {
     x : canvas.width / 2,
@@ -34,7 +23,7 @@ const user = {
 }
 
 // Oponent Paddle
-const com = {
+const opponent = {
     x : canvas.width - 10, // - width of paddle
     y : (canvas.height - 100) / 2, // -100 the height of paddle
     width : 10,
@@ -117,14 +106,12 @@ function collision(b, p){
 // update function, the function that does all calculations
 function update(){
     
-    // change the score of players, if the ball goes to the left "ball.x<0" computer win, else if "ball.x > canvas.width" the user win
+    // change the score of players, if the ball goes to the left "ball.x<0" AI opponent win, else if "ball.x > canvas.width" the user win
     if(ball.x - ball.radius < 0){
-        com.score++;
-        comScore.play();
+        opponent.score++;
         resetBall();
     }else if(ball.x + ball.radius > canvas.width){
         user.score++;
-        userScore.play();
         resetBall();
     }
     
@@ -132,27 +119,23 @@ function update(){
     ball.x += ball.velocityX;
     ball.y += ball.velocityY;
     
-    // computer plays for itself, and we must be able to beat it
+    // AI opponent plays for itself, and we must be able to beat it
     // simple AI
-    com.y += ((ball.y - (com.y + com.height/4)))*0.1;
+    opponent.y += ((ball.y - (opponent.y + opponent.height/4)))*0.1;
     
     // when the ball collides with bottom and top walls we inverse the y velocity.
     if(ball.y - ball.radius < 0 || ball.y + ball.radius > canvas.height){
         ball.velocityY = -ball.velocityY;
-        wall.play();
     }
     
-    // we check if the paddle hit the user or the com paddle
-    let player = (ball.x + ball.radius < canvas.width/2) ? user : com;
+    // we check if the paddle hit the user or the AI opponent paddle
+    let player = (ball.x + ball.radius < canvas.width/2) ? user : opponent;
     
     // if the ball hits a paddle
     if(collision(ball, player)){
-        // play sound
-        hit.play();
         // we check where the ball hits the paddle
         let collidePoint = (ball.y - (player.y + player.height/2));
         // normalize the value of collidePoint, we need to get numbers between -1 and 1.
-        // -player.height/2 < collide Point < player.height/2
         collidePoint = collidePoint / (player.height/2);
         
         // when the ball hits the top of a paddle we want the ball, to take a -45degees angle
@@ -167,7 +150,7 @@ function update(){
         ball.velocityY = ball.speed * Math.sin(angleRad);
         
         // speed up the ball everytime a paddle hits it.
-        ball.speed += 0.2;
+        ball.speed += 0.1;
     }
 }
 
@@ -180,8 +163,8 @@ function render(){
     // draw the user score to the left
     drawText(user.score,canvas.width/4,canvas.height/5);
     
-    // draw the COM score to the right
-    drawText(com.score,3*canvas.width/4,canvas.height/5);
+    // draw the AI opponent score to the right
+    drawText(opponent.score,3*canvas.width/4,canvas.height/5);
     
     // draw the net
     drawNet();
@@ -189,18 +172,14 @@ function render(){
     // draw the user's paddle
     drawRect(user.x, user.y, user.width, user.height, user.color);
     
-    // draw the COM's paddle
-    drawRect(com.x, com.y, com.width, com.height, com.color);
+    // draw the AI opponent's paddle
+    drawRect(opponent.x, opponent.y, opponent.width, opponent.height, opponent.color);
     
     // draw the ball
     drawArc(ball.x, ball.y, ball.radius, ball.color);
-}
-function game(){
-    update();
-    render();
-}
-// number of frames per second
-let framePerSecond = 50;
 
-//call the game function 50 times every 1 Sec
-setInterval(game, 1000 / framePerSecond);
+    update();
+    requestAnimationFrame(render);
+}
+
+render();
